39ea527a914304bfc7dd2354ab4bdccfa719d6c0
badar <badarshahzad54@gmail.com>
2016-11-19 16:16:44 +0500

    Week 6 functions learnging

---
+++ lab6.hs
@@ -0,0 +1,81 @@
+
+--Here is the quick sort
+
+add x y = x+y
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = [] 
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)  
+    | p x       = x : filter' p xs
+    | otherwise = filter' p xs
+	
+
+count :: a -> Int -> Int
+count x y = 1+y;
+
+
+
+
+{-
+filter'::(a->Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' f (x:xs)
+ | f x = x: filter' f(xs)
+ |otherwise = filter' f x
+
+-}
+
+{-
+quicksort :: (Ord a) => [a] -> [a]  
+quicksort [] = []  
+quicksort (x:xs) =   
+    let smallerSorted = quicksort [a | a <- xs, a <= x]  
+        biggerSorted = quicksort [a | a <- xs, a > x]  
+    in  smallerSorted ++ [x] ++ biggerSorted  
+
+--zip :: [a] -> [b] -> [(a,b)]
+--zip (a:as) (b:bs) = (a,b)
+--zip _      _      = []
+-}
+
+inc x = x+1
+
+incx x y = (x+1)*y
+
+inclist [] = []
+inclist (x:xs) = inc x: inclist(xs)
+
+multiply2 [] = []
+multiply2 (x:xs) = x*2:multiply2 xs;
+
+{-
+--inclistMultiply [] _ = []
+--inclistMultiply y  (x:xs) = incx x y : inclistMultiply xs
+-}
+
+zipwith' _ [] = []
+zipwith' f (x:xs)= f x : zipwith' f (xs)
+--zipwith1 f (muliply2 (x:xs)) = inc (multiply2 x): zipwith' f (xs)
+
+{-
+zipwithfilter::(Bool -> Bool) -> [a] -> Bool 
+zipwithfilter f (x:xs) = ( if (f (x) > xs == True) then zipwithfilter f (xs) else False
+-}
+
+{- 
+zipwithlist _ _ [] = []
+zipwithlist _ [] _ = []
+zipwithlist f (x:xs) (y:ys) = f x : f y : zipwithlist f (xs) (ys)
+-}
+
+{-
+findlargest f (x:xs)
+ | f x =  x/3829==0
+ | otherwise = findlargest f (xs) 
+-}
